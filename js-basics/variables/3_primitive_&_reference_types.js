ea6097126e2136d99557daf9671ad663265535a6
bautidea <juanbautistadeangelis@gmail.com>
2023-09-01 17:29:20 -0300

    Learning about dynamic typing and variable types.

--- js-basics/variables/3_primitive_&_reference_types.js
+++ js-basics/variables/3_primitive_&_reference_types.js
@@ -1,7 +1,7 @@
 /*  
     In js we have two types of variables:
         - Primitives / Value Types - String, Numbers, Booleans, undefined and null.
-        - Reference Types - We will talk about these later on.
+        - Reference Types - Object, Array, and Functions.
     In ES6 we also have the primitive type Symbol, we are going to see this type later
     on.
 */
@@ -10,3 +10,5 @@ let age = 30; // Number literal.
 let isApproved = true; // Boolean literal.
 let firstName; // Undefined.
 let lastName = null; // We use null when we want to explicitly clear the value of a variable.
+
+// Object, in JS and other programming languages is like an object in real life.
---
+++ js-basics/variables/4_dynamic_typing.js
@@ -0,0 +1,12 @@
+/*  
+    One thing that separates JS from other programming languages, is that
+    JS is a dynamic language.
+        In Static languages, when we decalte a variable, the type of
+            that variable is ser and it cannot be changed in the future.
+        In a Dynamic language, the type of a variable can change at run time.
+*/
+let myName = "Bauti";
+console.log(typeof myName);
+
+myName = 1;
+console.log(typeof myName);
