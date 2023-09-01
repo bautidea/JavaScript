39b0c993d6c219c9030ba1cddac67226d7638f96
bautidea <juanbautistadeangelis@gmail.com>
2023-09-01 17:37:34 -0300

    Learning about variable types

---
+++ js-basics/variables/3_primitive_and_reference_types.js
@@ -0,0 +1,34 @@
+/*  
+    In js we have two types of variables:
+        - Primitives / Value Types - String, Numbers, Booleans, undefined and null.
+        - Reference Types - Object, Array, and Functions.
+    In ES6 we also have the primitive type Symbol, we are going to see this type later
+    on.
+*/
+let name = "Bauti"; // String literal.
+let age = 30; // Number literal.
+let isApproved = true; // Boolean literal.
+let firstName; // Undefined.
+let lastName = null; // We use null when we want to explicitly clear the value of a variable.
+
+/*
+    Object, in JS and other programming languages is like an object in real life.
+    An object has properties. When we are dealing with multiple related variables,
+    we can put these variables inside an object.
+*/
+// These variables are part of a representation of a person.
+let myName = "Bauti";
+let myAge = 30;
+/*
+    Instead of declaring two variables, we can declare the person object. And instead of
+    referencing these two variables we can just reference the person object, and make our
+    code cleaner.
+*/
+let person = {
+  // Between braces we add one or more key-value pairs.
+  // keys are the properties of these objects.
+  name: "Bauti",
+  age: 30,
+}; // '{}' is called object literal.
+
+console.log(person);
