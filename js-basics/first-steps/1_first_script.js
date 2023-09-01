ebcb34885606b49561ce9ece3c36c3148a9df90b
bautidea <juanbautistadeangelis@gmail.com>
2023-09-01 08:21:14 -0300

    writing my 1st JS code

---
+++ js-basics/first-steps/1_first_script.js
@@ -0,0 +1,8 @@
+// We can write comments using this notation.
+/* Or this notation */
+/* 
+  What we have here is a statement. A statement is a piece of code that expresses an action
+  to be carried out. Here we want to "log" a message on the console.
+  All statements in JS should be terminated by ';'.
+*/
+console.log("Hello World");
