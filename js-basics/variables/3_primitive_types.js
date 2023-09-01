c46feec167872f6a516bc57c57f6b8bd0f4b4624
bautidea <juanbautistadeangelis@gmail.com>
2023-09-01 09:09:24 -0300

    Learning about primitive variables

---
+++ js-basics/variables/3_primitive_types.js
@@ -0,0 +1,12 @@
+/*  
+    In js we have two types of variables:
+        - Primitives / Value Types - String, Numbers, Booleans, undefined and null.
+        - Reference Types - We will talk about these later on.
+    In ES6 we also have the primitive type Symbol, we are going to see this type later
+    on.
+*/
+let name = "Bauti"; // String literal.
+let age = 30; // Number literal.
+let isApproved = true; // Boolean literal.
+let firstName; // Undefined.
+let lastName = null; // We use null when we want to explicitly clear the value of a variable.
