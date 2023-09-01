d1dd03c30c8ee4e7f5416cda19a6c88ac4beffad
bautidea <juanbautistadeangelis@gmail.com>
2023-09-01 17:45:14 -0300

    Lerning about Object Reference Type.

--- js-basics/variables/3_primitive_and_reference_types.js
+++ js-basics/variables/3_primitive_and_reference_types.js
@@ -12,7 +12,8 @@ let firstName; // Undefined.
 let lastName = null; // We use null when we want to explicitly clear the value of a variable.
 
 /*
-    Object, in JS and other programming languages is like an object in real life.
+    Object
+    In JS and other programming languages is like an object in real life.
     An object has properties. When we are dealing with multiple related variables,
     we can put these variables inside an object.
 */
@@ -30,5 +31,16 @@ let person = {
   name: "Bauti",
   age: 30,
 }; // '{}' is called object literal.
-
 console.log(person);
+
+// There are two ways to work with these properties.
+// Suppose we want to change this persons name, we can do this in two ways.
+
+// 1 - Dot Notation
+person.name = "Jhon";
+// We can use Dot Notation to read the value of a property.
+console.log(person.name);
+
+// 2 - Bracket Notation
+person["name"] = "Marcio";
+console.log(person["name"]);
