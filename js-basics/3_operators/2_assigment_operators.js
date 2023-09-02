5c3385f18d5d9e8113d9d54c6c349f18f0c289cf
bautidea <juanbautistadeangelis@gmail.com>
2023-09-02 12:00:21 -0300

    Assignment operators.

---
+++ js-basics/3_operators/2_assigment_operators.js
@@ -0,0 +1,17 @@
+/*  
+    'let' is an example of an assignment operator.
+*/
+let x = 10; // Using let to assign the value 10 to the variable.
+
+// The increment operator (x++;), is equivalent to this. But here we use
+//  the assignment operator to add 1 to 'x'.
+x = x + 1;
+x += 1;
+// If the value we want to add is bigger than 1, we can use an assignment
+// operator like this.
+x += 5;
+// For multiplication.
+x = x * 3;
+x *= 3;
+
+// Not all aritmetic operators have this combination with the assignment operator.
