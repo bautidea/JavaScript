ef7356db486651a6e45bdc6d287a1924a06b1cab
bautidea <juanbautistadeangelis@gmail.com>
2023-09-04 08:46:29 -0300

    Do-While loops.

--- js-basics/4_control_flow/3_loops.js
+++ js-basics/4_control_flow/3_loops.js
@@ -9,12 +9,14 @@
 */
 
 // For loops.
-// For loops need 3 statements,
-// - The first is called initialExpression (here we declare an initialized variable)
-// - The second is what we call a condition, we compare the value of the first expression
-//      with something else. The loop will run as long as this condition evaluates to true.
-// - The third part is called incrementExpression,we use this part to increment the value
-//      of the initialExpression once a loop is completed.
+/*
+  For loops need 3 statements,
+  - The first is called initialExpression (here we declare an initialized variable)
+  - The second is what we call a condition, we compare the value of the first expression
+      with something else. The loop will run as long as this condition evaluates to true.
+  - The third part is called incrementExpression,we use this part to increment the value
+      of the initialExpression once a loop is completed.
+*/
 for (let i = 0; i < 5; i++) {
   console.log("For Hello World");
 }
@@ -33,3 +35,18 @@ while (i < 5) {
   console.log("While Hello World");
   i++; // We increment i inside loop.
 }
+
+// Do-While loops
+/*  
+  They are very similar to while loops, but they have a difference, do-while loops are ALWAYS
+  executed at least once, even if the while-condition evaluates to false.
+  In while loops the condition get evaluated first and the the statement is executed.
+  In do-while loops the condition is evaluated at the end, that means that the statement is
+    always executed at least once, Even if the condition is false.
+  Just as while loops we have to declare the loop variable externally.
+*/
+i = 0;
+do {
+  console.log("Do-While Hello World");
+  i++;
+} while (i < 5);
