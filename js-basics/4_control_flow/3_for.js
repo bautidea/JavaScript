a514b8a1ceca1a7ffed4071b7e192740912a7f5e
bautidea <juanbautistadeangelis@gmail.com>
2023-09-04 08:22:45 -0300

    For loop.

---
+++ js-basics/4_control_flow/3_for.js
@@ -0,0 +1,20 @@
+/*  
+    In Js we have various kind of loops, and all these loops
+    essentially repeat an action a number of times. We have:
+    - For.
+    - While.
+    - Do...While.
+    - For...in.
+    - For...of.
+*/
+
+// For loops.
+// For loops need 3 statements,
+// - The first is called initialExpression (here we declare an initialized variable)
+// - The second is what we call a condition, we compare the value of the first expression
+//      with something else. The loop will run as long as this condition evaluates to true.
+// - The third part is called incrementExpression,we use this part to increment the value
+//      of the initialExpression once a loop is completed.
+for (let i = 0; i < 5; i++) {
+  console.log("Hello World");
+}
