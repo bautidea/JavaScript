af6315e7bf73c541d022c998ee25dc068c21444c
bautidea <juanbautistadeangelis@gmail.com>
2023-09-05 08:30:18 -0300

    Finished with ex1.

--- js-basics/excercises/1_control_flow/1_max_of_two_numbers.js
+++ js-basics/excercises/1_control_flow/1_max_of_two_numbers.js
@@ -1,11 +1,15 @@
 // Write a function that takes two numbers and return the maximum of the two.
 function max_number(numberA, numberB) {
   if (numberA > numberB) return numberA;
+  else if (numberA === numberB) return "equal numbers";
   else return numberB;
+
+  // A simpler aproach. Using ternary operator.
+  // return (numberA > numberN) ? numberA : numberB
 }
 
-let firstNumber = 25;
+let firstNumber = 50;
 let secondNumber = 50;
 let max_num = max_number(firstNumber, secondNumber);
 
-console.log("The maximum number is", max_num);
+console.log("The maximum number is -", max_num);
