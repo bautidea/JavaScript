86d29534c18e9a576d86ce998ad64dbb488d5cb1
bautidea <juanbautistadeangelis@gmail.com>
2023-09-07 08:06:17 -0300

    Tweaking function.

--- js-basics/excercises/1_control_flow/5_even_odd_number.js
+++ js-basics/excercises/1_control_flow/5_even_odd_number.js
@@ -4,11 +4,11 @@
  * Indicates if each number is "EVEN" or "ODD".
  * @date 2023-09-07
  * @param { Number } limit
- * Located in line {@link showNumbers}
+ * {@link showNumbers}
  */
 function showNumbers(limit) {
   for (let i = 0; i <= limit; i++) {
-    let evenOrOdd = i % 2 ? "ODD" : "EVEN";
+    let evenOrOdd = i % 2 === 0 ? "EVEN" : "ODD";
 
     console.log(`${i} - ${evenOrOdd}`);
   }
