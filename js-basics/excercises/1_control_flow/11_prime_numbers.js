c66aed34c9afaf10d9828129a8fcf8745f6dbaf6
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-08 08:56:31 -0300

    Starting with ex11.

---
+++ js-basics/excercises/1_control_flow/11_prime_numbers.js
@@ -0,0 +1,12 @@
+/**
+ * This function takes a limit, and show all primes number from 0 to that limit.
+ * A prime number is a number that can only be divided by 1 and iself.
+ *  {@link showPrimes}
+ *
+ * @date 2023-09-08
+ *
+ * @param { number } limit
+ */
+function showPrimes(limit) {}
+
+showPrimes(20);
