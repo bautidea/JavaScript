8abbc80b932197e8d5006651761f991f6d0a48ef
bautidea <juanbautistadeangelis@gmail.com>
2023-09-08 16:51:21 -0300

    Finished learning about factory functions.

--- js-basics/5_objects/2_factory_functions.js
+++ js-basics/5_objects/2_factory_functions.js
@@ -7,6 +7,9 @@ let circle = {
     x: 2,
     y: 3,
   },
+  move: function () {
+    console.log("Moving circle...");
+  },
 };
 
 let circle2 = {
@@ -15,8 +18,34 @@ let circle2 = {
     x: 2,
     y: 3,
   },
+  move: function () {
+    console.log("Moving circle...");
+  },
 };
 
 /**
- * ! the problem we have here
+ * ! the problem we have here is that we have duplicated the implementation
+ * ! of draw method.
+ * * If our objects have logic we need different ways to create objects.
+ * Thats when we use
+ * ! Factory or constructor functions.
  */
+
+// Factory function.
+// Whenever we call this function we'll get a circle object.
+function createCircle(radius) {
+  return {
+    // In modern JS if our key and value are the same, we can declare a property like this.
+    radius, // Same as radius : radius
+
+    // Instead of declaring the function as a key : value pair, we can define it like this
+    draw() {
+      console.log("Drawing circle...");
+    },
+  };
+}
+
+// We call this function to create a circle object.
+let cirlce3 = createCircle(5);
+console.log(cirlce3);
+cirlce3.draw();
