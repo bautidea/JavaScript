d662ed1328c6c47cda73b2616bf2042443a3050e
bautidea <juanbautistadeangelis@gmail.com>
2023-09-08 16:26:18 -0300

    Tweaking

---
+++ js-basics/5_objects/2_factory_functions.js
@@ -0,0 +1,22 @@
+// In the previous file we created an object usign Object Literal Syntax,
+// but there's a problem here.
+// Imagine if we wanted to create two cirlce objects. Like this:
+let circle = {
+  radius: 1,
+  location: {
+    x: 2,
+    y: 3,
+  },
+};
+
+let circle2 = {
+  radius: 1,
+  location: {
+    x: 2,
+    y: 3,
+  },
+};
+
+/**
+ * ! the problem we have here
+ */
