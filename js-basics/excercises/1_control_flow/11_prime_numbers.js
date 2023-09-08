5b5073c12b4e2d7bb82912865a304c56daec7ebd
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-08 09:53:43 -0300

    Finished with ex 11, first implementation. -> need to implement changes.

--- js-basics/excercises/1_control_flow/11_prime_numbers.js
+++ js-basics/excercises/1_control_flow/11_prime_numbers.js
@@ -7,6 +7,30 @@
  *
  * @param { number } limit
  */
-function showPrimes(limit) {}
+function showPrimes(limit) {
+  if (limit === 0) return console.log("Limit cant be 0");
+
+  for (let number = 1; number <= limit; number++) {
+    if (number === 2 || number === 3) console.log(number);
+    if (number % 2 != 0 && number % 3 != 0) console.log(number);
+  }
+}
+
+// Another implementation.
+function showPrimes2(limit) {
+  for (number = 1; number <= limit; number++) {
+    let isPrime = true;
+
+    for (factor = 2; factor < number; factor++) {
+      if (number % factor === 0) {
+        isPrime = false;
+        break;
+      }
+    }
+
+    if (isPrime) console.log(number);
+  }
+}
 
 showPrimes(20);
+showPrimes2(20);
