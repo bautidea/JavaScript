f57a5426120513e1fce2611010c4dfc6bca67b96
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-13 08:19:27 -0300

    Finished.

--- js-basics/6_arrays/3_arrow_function.js
+++ js-basics/6_arrays/3_arrow_function.js
@@ -10,4 +10,10 @@ const course = courses.find(function (course) {
 });
 console.log(course); // --> { id: 1, name: 'a'}
 
-// ! In ES6 there is a shorter and a cleaner way to write the same code using arrow function
+// ! In ES6 there is a shorter and a cleaner way to write the same code using 'arrow functions'.
+// Whenever we want to pass a callback function or as an argument we can use this syntax.s
+// Remove the 'function' keyword.
+// And to separate the parameters from it's body we use a fat arrow '=>'.
+// If function has a single line of code we can drop the '{}'.
+const course1 = courses.find((course) => course.name === 'a'); // --> arrow function.
+console.log(course1); // --> { id: 1, name: 'a'}
