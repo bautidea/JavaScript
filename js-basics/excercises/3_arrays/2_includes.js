1fe72d200b0d4b8cd80af0ec7ba695debeb16a97
bautidea <juanbautistadeangelis@gmail.com>
2023-09-17 12:10:23 -0300

    Finished ex2.

---
+++ js-basics/excercises/3_arrays/2_includes.js
@@ -0,0 +1,15 @@
+// In this excercise im going to check if in a given array it includes
+// a given number passed as input. Just like the '.include()' method.
+
+const arrayNumbers = [1, 2, 3, 4, 5];
+
+function isIncluded(array, number) {
+  for (let n of array) {
+    if (number === n) return true;
+  }
+
+  return false;
+}
+
+console.log(isIncluded(arrayNumbers, 2)); // --> true
+console.log(isIncluded(arrayNumbers, 14)); // --> false
