dfd2a236acab4a6e86598cc474cff6e40340be94
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-19 07:28:07 -0300

    Finished.

--- js-basics/7_functions/1_declarations_vs_expressions.js
+++ js-basics/7_functions/1_declarations_vs_expressions.js
@@ -1,6 +1,16 @@
 // Defining a function.
 // In js there are two ways of declaring a function.
 
+// The key difference between these two functions, is that we can call
+// the function defined using 'Function declaration' syntax, before
+// it is defined. Same as a variable.
+// The reason for this is because when our JS engine moves all the
+// functions declarations to the top.
+// * This is called Hoisting, the process of moving functions declarations
+// * to the top of the file. This is done automatically by the Js engine.
+
+walk(); // --> walk
+
 // * Function declarations
 function walk() {
   console.log('walk');
@@ -8,13 +18,16 @@ function walk() {
 
 // * Function expression
 // Same as declaring a variable.
+
+// * run(); // --> Cannot access 'run' before initialization
+
 // We have an Anonymous Function Expression, when the name is
 // not passed.
 let run = function () {
   console.log('run');
 };
 // We call the anonymous function like we call a function
-run();
+run(); // --> run
 
 // Named Function Expression
 let runing = function runing() {
