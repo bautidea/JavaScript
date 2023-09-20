98e8db94c328ae9c4cdfe0193b750d94768b8512
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-20 15:14:55 -0300

    Finished.

--- js-basics/7_functions/10_changing_this.js
+++ js-basics/7_functions/10_changing_this.js
@@ -51,5 +51,39 @@ playVideo.apply({ name: 'bauti' }, [1, 2]); // --> { name: 'bauti' }
 // Since it returns a new function we can store the result in a variable or constant.
 const newPlayVideo = playVideo.bind({ name: 'bauti' });
 newPlayVideo(); // --> { name: 'bauti' }
-
 // Or we can immediately call the function that is returned
+playVideo.bind({ name: 'bauti' })();
+
+// To apply this to the previous example.
+// We are going to use the '.bind()' method inmediately after declaring the callback
+// function, and then pass an object to be used as the value of 'this'.
+// The value we pass is 'this' because at that point we are at the 'showTags()' method
+// of 'video2' object, so 'this' will point to the video2 object.
+const video2 = {
+  title: 'a',
+  tags: ['b', 'c', 'd'],
+  showTags() {
+    this.tags.forEach(
+      function (tag) {
+        console.log(this.title, tag);
+      }.bind(this)
+    );
+  },
+};
+video2.showTags(); // --> a b, a c, a d
+
+// ! There is a newer an better solution. Starting from ECMA script 6 we have
+// ! arrow function.
+// The good thing about arrow function is that they inherit the 'this' value,
+// from the containing function. In the callback function 'this' references
+// the video3 object.
+const video3 = {
+  title: 'a',
+  tags: ['b', 'c', 'd'],
+  showTags() {
+    this.tags.forEach((tag) => {
+      console.log(this.title, tag);
+    });
+  },
+};
+video3.showTags(); // --> a b, a c, a d
