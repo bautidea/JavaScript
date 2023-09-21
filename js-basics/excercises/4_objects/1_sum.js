3cb9c7ca3435329b90802c97cf8b060087b416d3
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-21 08:42:57 -0300

    Finished ex1 :)

--- js-basics/excercises/4_objects/1_sum.js
+++ js-basics/excercises/4_objects/1_sum.js
@@ -2,4 +2,13 @@
 // all the elements passed as input.
 // The number of inputs can vary.
 
-function sum(...args) {}
+function sum(...args) {
+  // checking if 1st element is an array.
+  const isArray = Array.isArray(args[0]);
+
+  if (isArray) args = [...args[0]];
+
+  return args.reduce((acum, current) => acum + current);
+}
+
+console.log(sum([1, 2, 3, 4]));
