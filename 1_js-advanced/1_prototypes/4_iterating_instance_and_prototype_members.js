e97fe476dfcc702128ae43cc6ee99ef2b1d2f556
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-09-25 07:45:33 -0300

    Finished.

--- 1_js-advanced/1_prototypes/4_iterating_instance_and_prototype_members.js
+++ 1_js-advanced/1_prototypes/4_iterating_instance_and_prototype_members.js
@@ -1,4 +1,4 @@
-// One thing to have in mind is that is doesnt matter when you change the prototype.
+// We are going to see how to iterate over instance vs prototype properties.
 function Circle(radius) {
   // Instance members
   this.radius = radius;
@@ -8,17 +8,22 @@ function Circle(radius) {
   };
 }
 
-// We can create an object first, and then modify the prototype.
-const c2 = new Circle(1);
+const c1 = new Circle(1);
 
 // Prototype members.
 Circle.prototype.draw = function () {
   console.log('draw');
 };
 
-const c1 = new Circle(1);
+// * 'Object.keys()' ONLY returns instance keys.
+console.log(Object.keys(c1)); // --> [ 'radius', 'move' ]
 
-// We can see that the draw method is available, because we are dealing with objects references,
-// we have a sinlge object in memory, as soon as we modify that the changes are applied
-// immediatly.
-c2.draw(); // --> draw
+// * 'for...in' loop returns ALL members
+for (let key in c1) console.log(key); // --> radius, move, draw
+
+// In JS language we often use the word 'own' instead of instance. Referring to
+// instance members as 'own' members.
+// The next method will return if a object has a certain 'own' property.
+console.log(c1.hasOwnProperty('radius')); // --> true
+// We get false for 'draw' method, because its a prototype member.
+console.log(c1.hasOwnProperty('draw')); // --> false
