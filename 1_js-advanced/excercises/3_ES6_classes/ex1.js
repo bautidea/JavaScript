2da7e451b7260d11a7484e5cd2c39228f70ca936
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-10-03 08:27:52 -0300

    Finish Excercise.

--- 1_js-advanced/excercises/3_ES6_classes/ex1.js
+++ 1_js-advanced/excercises/3_ES6_classes/ex1.js
@@ -4,17 +4,37 @@
 //  - pop --> Removing the top object of the 'box'.
 //  - peek --> Returns the object on top of the stack.
 const _elements = new WeakMap();
-
 class Stack {
   constructor() {
-    this[_elements] = [];
-    this.count = _elements.length;
+    _elements.set(this, []);
   }
 
   push(value) {
-    _elements.push(value);
+    _elements.get(this).push(value);
+  }
+
+  pop() {
+    if (this.count === 0) throw new Error('Stack is empty');
+    _elements.get(this).pop();
+  }
+
+  peek() {
+    if (this.count === 0) throw new Error('Stack is empty');
+    return _elements.get(this).slice(-1)[0];
+  }
+
+  get count() {
+    return _elements.get(this).length;
   }
 }
 
 const stack = new Stack();
 stack.push('a');
+console.log(stack.peek()); // --> a
+stack.push('b');
+console.log(stack.peek()); // --> b
+console.log(stack.count); // --> 2
+stack.pop();
+console.log(stack.peek()); // --> a
+stack.pop();
+console.log(stack.peek()); // --> Stack is empty
