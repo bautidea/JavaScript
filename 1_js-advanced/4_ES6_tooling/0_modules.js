a74195f9cdc8f6e8e5733264a31e575014df6055
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-10-03 08:55:28 -0300

    Finish.

--- 1_js-advanced/4_ES6_tooling/0_modules.js
+++ 1_js-advanced/4_ES6_tooling/0_modules.js
@@ -6,21 +6,32 @@
 //  - Reuse --> We can reuse these modules in differents applications.
 //  - Abstract code --> Wich means we can hide some of the complexity in a module, and only expose
 //                      the essentials.
-const _radius2 = new WeakMap();
-class Circle2 {
+const _radius = new WeakMap();
+class Circle {
   constructor(radius) {
     _radius.set(this, radius);
   }
 
-  get radius() {
-    return _radius.get(this);
-  }
-
   // We can also define a setter really easily.
-  set radius(value) {
-    if (value <= 0) throw new Error('Invalid radius');
-    _radius.set(this, value);
+  draw() {
+    console.log(`Circle with radius ${_radius.get(this)}`);
   }
 }
 
-const c2 = new Circle2(8);
+const c = new Circle(8);
+c.draw();
+
+// Here we are using a weakmap to implement a private property in our class. So when we create
+// the circle object we dont have access to the radius property (c.radius --> does not exists).
+// However in this code i have access to the WeakMap, so i can read the '_radius' private property
+console.log(_radius.get(c)); // --> 8
+// What we should do here is to take the Circle class and the radius WeakMap and put it in a separate file (a module)
+// and then only expose the cirle class to the outside.
+// And with that we wont have access to the WeakMap.
+
+// * Popular Module Formats:
+//   - AMD - Asynchronus Module Definition. --> Primarily used in browser applications.
+// * - CommonJS --> Used in Node.
+//   - UMD - Universal Module Definition. --> Can be used both in browser and Node.js.
+// * - ES6 Modules --> From ES6 JS native supports module format. Its used in browsers.
+// Out of this list we are going to focus on CommonJS and ES6 Modules.
