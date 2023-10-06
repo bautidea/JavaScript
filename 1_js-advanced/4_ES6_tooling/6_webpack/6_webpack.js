5d3e5a06bc008736d583a4ac2a9a26461e27f437
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-10-06 17:09:55 -0300

    Working on last lection.

--- 1_js-advanced/4_ES6_tooling/6_webpack/6_webpack.js
+++ 1_js-advanced/4_ES6_tooling/6_webpack/6_webpack.js
@@ -1,11 +1,11 @@
 // ! We are going to see the workflow that we should use on a day to day basis.
 // We have two modules (index.js and circle.js)
-// We open up command prompt or terminal and run
-// * npm i webpack-cli
-// Im going to use version --> npm i webpack@2.0.14
+// We open up command prompt or terminal and run (after running npm init --yes)
+// * npm i -g webpack-cli
+// Im going to use version --> npm i -g webpack-cli@2.0.14
+// This webpack version need to be installed globaly (-g) in order to run webpack-cli init
 
-// In the project folder we run
-// * webpack-cli init
-// -g means that we are goint to install this globally.
-// npm uninstall -g webpack -->  to uninstall
-// This will ask us a bounch of questions, and based on this it will cerate a web configuration file.
+// In the project folder we run, in the terminal
+// * npx webpack-cli init
+// This will ask us a bounch of questions, and based on this it will cerate a webpack configuration file.
+// Which of the following JS solutions do you want to use? ES6
