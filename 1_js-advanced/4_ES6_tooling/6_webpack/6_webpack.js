1c504515f6a97f400ab9ded9af2788bb62520b27
BautiDeAngelis <juanbautistadeangelis@gmail.com>
2023-10-06 09:13:19 -0300

    5 mins to finish course.

---
+++ 1_js-advanced/4_ES6_tooling/6_webpack/6_webpack.js
@@ -0,0 +1,11 @@
+// ! We are going to see the workflow that we should use on a day to day basis.
+// We have two modules (index.js and circle.js)
+// We open up command prompt or terminal and run
+// * npm i webpack-cli
+// Im going to use version --> npm i webpack@2.0.14
+
+// In the project folder we run
+// * webpack-cli init
+// -g means that we are goint to install this globally.
+// npm uninstall -g webpack -->  to uninstall
+// This will ask us a bounch of questions, and based on this it will cerate a web configuration file.
